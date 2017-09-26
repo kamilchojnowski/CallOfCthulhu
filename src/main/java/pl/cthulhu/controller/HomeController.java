335c88017f7ad4936d7ea667a606e0c664c9eb8a
kamilchojnowski <kamil.chojnowski95@gmail.com>
2017-09-26 17:40:31 +0200

    work

--- src/main/java/pl/cthulhu/controller/HomeController.java
+++ src/main/java/pl/cthulhu/controller/HomeController.java
@@ -2,13 +2,11 @@ package pl.cthulhu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-	@GetMapping({"", "/home"})
-	@ResponseBody
+	@GetMapping({"", "/home", "/"})
 	public String home(){
 		return "home";
 	}
