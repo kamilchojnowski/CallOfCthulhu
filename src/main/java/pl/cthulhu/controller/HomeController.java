5e08fe46aa0c3193ef18fc040d2a7ceffcb4426d
kamilchojnowski <kamil.chojnowski95@gmail.com>
2017-09-27 18:33:10 +0200

    backup members

--- src/main/java/pl/cthulhu/controller/HomeController.java
+++ src/main/java/pl/cthulhu/controller/HomeController.java
@@ -2,12 +2,24 @@ package pl.cthulhu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
 	@GetMapping({"", "/home", "/"})
 	public String home(){
+		return "home";
+	}
+	
+	@GetMapping("/members")
+	public String members(){
+		return "members";
+	}
+	
+	@PostMapping("/members")
+	public String membersPost(){
+		
 		return "home";
 	}
 
