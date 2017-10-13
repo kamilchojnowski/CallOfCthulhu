b84a245538a6d0a9f0c21191e7921737a399a4fe
kamilchojnowski <kamil.chojnowski95@gmail.com>
2017-10-13 19:09:28 +0200

    chat usage

--- src/main/resources/static/script/plan.js
+++ src/main/resources/static/script/plan.js
@@ -1,12 +1,30 @@
 document.addEventListener("DOMContentLoaded",function(event) {
+	
 	var PC = "#locationError:30°15'16.2\"N 89°52'31.2\"W";
 	var BG = "JMLocal";
 	var submitmsg = $("#submitmsg");
 	var chatbox = $("#chatbox");
 	var msg = $("#msg");
-	$(submitmsg).on("click", function(){
+	
+	function PcMsg (message){
+		var temp = $(chatbox).val() + "From '" + PC + "': " + message + "\n" ;
+		$(chatbox).val(temp);
+	}
+	
+	function BgMsg(){
 		var message = $(msg).val();
 		$(msg).val("");
-		$(chatbox).val(message);
+		var temp = $(chatbox).val() + "From '" + BG + "': " + message + "\n" ;
+		$(chatbox).val(temp);
+	}
+	
+	$(msg).keypress(function(e) {
+	    if(e.which == 13) {
+	        BgMsg();
+	        e.preventDefault();
+	    }
 	});
+	
+	$(submitmsg).on("click", BgMsg);
+	
 });
