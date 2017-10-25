2a9ed5d84c2a712d1c29eab53232bf641cd3d83a
kamilchojnowski <kamil.chojnowski95@gmail.com>
2017-10-25 12:59:15 +0200

    TypeIt save

--- src/main/resources/static/script/final.js
+++ src/main/resources/static/script/final.js
@@ -1,5 +1,52 @@
 $('#write').typeIt({
-    strings: 'This is a simple string.',
-    speed: 50,
+    strings: 'Error code #211034526,',
+    speed: 30,
     autoStart: true
-});
+})
+.tiPause(500)
+.tiBreak()
+.tiType('Error couse: ExternUsageOutOfData.')
+.tiPause(1000)
+.tiBreak()
+.tiType('System force close of process(es):')
+.tiPause(1000)
+.tiSettings({Speed: 0})
+.tiBreak()
+.tiType('#117342560000')
+.tiBreak()
+.tiType('#117342560001')
+.tiBreak()
+.tiType('#117342560002')
+.tiBreak()
+.tiType('#117342560003')
+.tiBreak()
+.tiType('#102300060000')
+.tiBreak()
+.tiType('#102300060001')
+.tiBreak()
+.tiType('#102300060002')
+.tiBreak()
+.tiType('#000000000000')
+.tiBreak()
+.tiType('#000000000001')
+.tiSettings({Speed: 30})
+.tiPause(1000)
+.tiBreak()
+.tiType('There is no hope')
+
+
+
+
+
+
+
+/*
+
+.tiBreak()
+.tiType('#117342560000')
+
+.tiPause(1000)
+.tiBreak()
+.tiType('')
+
+*/
